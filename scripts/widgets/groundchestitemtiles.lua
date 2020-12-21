03fffb15ce38413f7f2bc0ebe1ac2a5b737f7720
sauktux <64164791+sauktux@users.noreply.github.com>
2020-12-21 21:41:51 +0200

    Slot implementation and Test
    groundchestitemtiles.lua updated to perform its functions.
    groundchestui.lua test added and a singular slot added for testing purposes.

--- scripts/widgets/groundchestitemtiles.lua
+++ scripts/widgets/groundchestitemtiles.lua
@@ -5,21 +5,158 @@ local ImageButton = require "widgets/imagebutton"
 local UIAnim = require "widgets/uianim"
 local UIAnimButton = require "widgets/uianimbutton"
 
-local GroundItemTile = Class(Widget,function(self,owner,atlas,tex,count,bg)
+
+--GroundItemTile Class: Holds all item information, is responsible for removing itself when the item entity can't be found anymore. Is NOT responsible for sorting.
+--For optimization and to reduce lag, perhaps all tiles should exist and stay, but be hidden if they don't have any item?
+local GroundItemTile = Class(Widget,function(self,item,bg,atlas,tex,count)
         Widget._ctor(self,"GroundItemTile")
 		
-        self.owner = owner
-        self.atlas = atlas
-        self.tex = tex
-        self.count = count or 1
-		self.bg = bg
-        
-        --self:StartUpdating()
-    end
-    )
-    
-function GroundItemTile:OnUpdate(dt)
+        self.item = item --To keep track of its existence.
+        self.atlas = atlas --Item atlas
+        self.tex = tex --Item tex
+        self.count = count or 1 --Item count(If it's stackable)
+		self.stackable = false --Value gets updated elsewhere.
+		self.chestitem = nil--Compatibility with Chest Memory mod.
+		self.container = nil--Compatibility with Chest Memory mod.
+		self.chestslot = nil--Compatibility with Chest Memory mod.
+		
+		self.bg = bg or {atlas = "images/quagmire_recipebook.xml", tex = "cookbook_known.tex", scale = 0.4}--Used bg should be something that centers its widgets at (0,0). Value "bg" should hold atlas,tex,scale.
+		--global_redux.tex has some nice images, could any of them be used to convey information?
+		--[[ Backgrounds: 
+		//quagmire_recipebook.xml - ingredient_slot.tex; recipe_known.tex, cookbook_known.tex,cookbook_known_selected.tex --recipe_known looks cool, but doesn't fit bg colour.
+		//ui.xml - portrait_bg.tex; in-window_button_tile_disabled.tex ; in-window_button_tile_idle.tex;
+		//hud.xml - inv_slot.tex ; 
+        --]]
+		local focus_scl = 1.2
+		self.onhover_scale = {focus_scl,focus_scl,focus_scl}
+		
+		local normal_scl = 1
+		self.nohover_scale = {normal_scl,normal_scl,normal_scl}
+		
+		self.widget_scale = self.bg.scale or 1 --Overall widget scale
+		
+		-- color: R,G,B,Opacity
+		self.onhover_color = {1,1,1,1}
+		self.nohover_color = {1,1,1,1}
+		self.disabled_color = {1,1,1,1}
+		
+		self.focus_sound = nil --Sound which will be played when the tile is hovered over.
+		-- I think it's more annoying than useful, so no use.
+		
+		self.item_bg = self:AddChild(ImageButton(self.bg.atlas,self.bg.tex))
+		
+		self.item_bg:SetNormalScale(self.nohover_scale[1],self.nohover_scale[2],self.nohover_scale[3])
+		self.item_bg:SetFocusScale(self.onhover_scale[1],self.onhover_scale[2],self.onhover_scale[3])
+		
+		self.item_bg:SetImageNormalColour(unpack(self.nohover_color))
+		self.item_bg:SetImageFocusColour(unpack(self.onhover_color))
+		self.item_bg:SetImageDisabledColour(unpack(self.disabled_color))
+		
+		self.item_bg:SetFocusSound(self.focus_sound)
+		
+		self.item_bg:Show()
+		
+		self.item_display = self.item_bg:AddChild(ImageButton(self.atlas,self.tex))
+		--The settings of scaling, colours, etc. should move over from "self.item_bg" as "self.item_display" is the child.
+		
+		self.item_display:SetScale(2,2,2) -- The item is rather small compared to the tile itself.
+		
+		
+		self.count_text = self.item_bg:AddChild(Text(NUMBERFONT,42))
+		self.count_text:SetPosition(2,16) --Default position for an item that's in an "inventory slot"s.
+		
+		self.item_bg:SetOnGainFocus(function() self.count_text:SetSize(42*focus_scl) self.count_text:SetPosition(2,16) end)
+		self.item_bg:SetOnLoseFocus(function() self.count_text:SetSize(42) self.count_text:SetPosition(2,16*focus_scl) end)
 
+		self:SetOnClickFn(function() print("*Click*") end)
+		self:SetScale(self.widget_scale)
+		self:Show()
+		self:UpdateStackText()
+        self:StartUpdating()
+		
+		
+    end
+)
+
+function GroundItemTile:SetOnClickFn(fn)
+	self.item_bg:SetOnClick(fn)
+end
+
+function GroundItemTile:RemoveItem()
+	self.item = nil
+	self.atlas = nil
+	self.tex = nil
+	self.count = 1
+	self.stackable = false
+	self.chestitem = nil
+	self.container = nil
+	self.chestslot = nil
+	self.item_display:SetTextures("images/quagmire_recipebook.xml","coin_unknown.tex")
+	self.item_display:Hide()
+	self:UpdateStackText()
+	self:StopUpdating()
+end
+
+function GroundItemTile:SetItem(item,atlas,tex,container,slot)
+	if self.item == item then return end
+	self.item = item
+	self.atlas = atlas
+	self.tex = tex
+	self.chestitem = container ~= nil
+	self.container = container
+	self.chestslot = slot
+	self.item_display:SetTextures(atlas,tex)
+	self.item_display:Show()
+	self:StartUpdating()
+end
+    
+function GroundItemTile:UpdateItemStacks()
+	if self.item then
+		if self.chestitem then
+			self.stackable = self.item._stackable
+			self.count = self.item.stacksize
+		else
+			self.stackable = self.item and self.item.components and self.item.components.stackable
+			self.count = self.stackable and self.stackable:StackSize() or 1
+		end
+	end
+end
+	
+function GroundItemTile:UpdateStackText()
+	if not self.item then
+		self.count_text:SetString("NO ITEM")
+		self.count_text:Hide()
+	elseif self.item and self.stackable then
+		self.count_text:Show()
+		self.count_text:SetString(tostring(self.stackable))
+	end
+end
+	
+function GroundItemTile:ValidateItem()
+	if self.chestitem then
+		if self.container and self.container.CS_contents then
+			local myitem = self.container.CS_contents[self.chestslot]
+			if myitem and myitem.prefab == self.item.prefab then
+				self.item = myitem --To update the stackable component.
+				return true
+			end
+		end
+		return nil
+	else
+		return self.item and self.item:IsValid() and not self.item:HasTag("INLIMBO")
+	end
+end
+
+function GroundItemTile:HasItem()
+	return self.item ~= nil
+end
+
+function GroundItemTile:OnUpdate(dt)
+	if not self:ValidateItem() then
+		self:RemoveItem()
+	end
+	self:UpdateItemStacks()
+	self:UpdateStackText()
 end
 
 return GroundItemTile
--- scripts/widgets/groundchestui.lua
+++ scripts/widgets/groundchestui.lua
@@ -6,10 +6,12 @@ local UIAnim = require "widgets/uianim"
 local UIAnimButton = require "widgets/uianimbutton"
 local GroundChestItemTiles = require "widgets/groundchestitemtiles"
 
-local screen_x, screen_y,half_x,half_y
+local screen_x, screen_y,half_x,half_y,w,h
 
 local min_seen = 8 --How much of the widget can minimally be seen when it's moved out of your screen at x or y coordinates?
 --I think 8 is around the size where you might not be able to drag it out.
+local items_page = 50--10x wide and 5x tall seems balanced to both see the item and its number. And it leaves space up top for the additional controls.
+--Additional controls like: Switch views, Switch pages, etc.
 
 local on_button_press_fn
 
@@ -28,7 +30,9 @@ local function InGame()
 	return ThePlayer and ThePlayer.HUD and not ThePlayer.HUD:HasInputFocus()
 end
 
-
+--Some features:
+--Clicking on an item tile: Will queue up for the item to be picked up, several tiles can be clicked at once and they will be picked up based on clicking order.
+--There are several variants for the background of the item tile.
 
 local GroundChestUI = Class(Widget,function(self,owner)
         Widget._ctor(self,"GroundChestUI")
@@ -40,18 +44,23 @@ local GroundChestUI = Class(Widget,function(self,owner)
 		on_button_press_fn = function() self:Toggle() end
 		
         self.owner = owner
+		self.tiles = {} --Track all tiles that currently exist and aren't deleted.
 		
 		self.pos_x = half_x--Centered
-		self.pos_y = half_y*1.5--75% up
+		self.pos_y = half_y*1.5--At a 0.75/1 position from below.
 		self.offset_x = 0
 		self.offset_y = 0
 		
-		self.size_x = 1/10*half_x+64*6
+		self.size_x = 1/10*half_x+64*7 --Not sure why I use 1/10 of screen size and then add 64's, but it seems like a relatively good size.
 		self.size_y = 1/10*half_y+64*5
 		
 		self.bg = self:AddChild(Image("images/plantregistry.xml", "backdrop.tex"))
 		self:SetPosition(self.pos_x+self.offset_x,self.pos_y+self.offset_y)
 		self.bg:SetSize(self.size_x,self.size_y)
+		
+		self.test_tile = self.bg:AddChild(GroundChestItemTiles())
+		--self:FillBoard(0.4)--Test function, to be removed when adding tiles in proper way.
+		--Should be replaced via the Widget GroundChestItemTiles.
 		
 		local ongainfocus_fn = function() self.focused = true end
 		local onlosefocus_fn = function() self.focused = false end
@@ -65,6 +74,46 @@ local GroundChestUI = Class(Widget,function(self,owner)
         self:StartUpdating()
     end)
 
+function GroundChestUI:FillBoard(scale)--Function for testing, use as reference, do not use for anything other than testing.
+		self.bgitem = self.bg:AddChild(ImageButton("images/quagmire_recipebook.xml","cookbook_known.tex"))
+		self.bgitem:SetPosition(0,0)
+		self.bgitem:SetOnClick(function() print("Clickity Click") end)
+		self.bgitem:Hide()
+		w,h = self.bgitem:GetSize()
+		w = w*scale
+		h = h*scale
+		for x = 1,math.floor(self.size_x/w) do
+			for y = 1,math.floor(self.size_y/h)-2 do
+			self.bgitem = self.bg:AddChild(ImageButton("images/quagmire_recipebook.xml","recipe_known.tex"))
+			self.bgitem:SetScale(scale)
+			
+			local min_vx = -8 -- Min distance it has to be from the vertical edges
+            local u_x = self.size_x-2*min_vx
+            local d_x = u_x/(math.floor(self.size_x/w+1))
+			
+            local min_tz = 32 -- Min top
+            local min_bz = 8 -- Min bot
+            local u_z = self.size_y-min_tz-min_bz
+            local d_z = u_z/(math.floor(self.size_y/h-2)+1)
+            self.bgitem:SetPosition((-0.5)*u_x+d_x*x,(0.5)*u_z-min_tz-d_z*y)
+			
+			self.bgitem:SetOnClick(function() print("Clickity Click") end)
+			self.bgitem:Show()
+		
+			self.testitem = self.bgitem:AddChild(ImageButton(GetInventoryItemAtlas("cane_ancient.tex"),"cane_ancient.tex"))
+			self.testitem:SetScale(2)
+			self.testitem:Show()
+			self.testitemcount = self.bgitem:AddChild(Text(NUMBERFONT,72))
+			self.testitemcount:SetPosition(0,-30)
+			self.testitemcount:SetString(tostring(math.random(1,40)))
+			self.testitemcount:MoveToFront()
+			self.testitemcount:Show()
+			self.bgitem:SetOnGainFocus(function() self.testitemcount:SetSize(72*1.2) self.testitemcount:SetPosition(0,-30*1.2) end)
+			self.bgitem:SetOnLoseFocus(function() self.testitemcount:SetSize(72) self.testitemcount:SetPosition(0,-30) end)
+			end
+		end
+end
+
 function GroundChestUI:Toggle()
 	if self.shown then
 		self.shown = false
@@ -74,7 +123,18 @@ function GroundChestUI:Toggle()
 		self:Show()
 	end
 end
-    
+	
+function GroundChestUI:AddItem(prefab)
+	
+	if not prefab or prefab == "" then return nil end
+	
+	
+	
+end
+
+ 
+ 
+ 
 function GroundChestUI:UpdatePosition()
 	self:SetPosition(self.pos_x+self.offset_x,self.pos_y+self.offset_y)
 end
@@ -103,9 +163,19 @@ function GroundChestUI:HandleMouseMovement()
 	end	
 end
 
+function GroundChestUI:GetNearestItem()
+	local pos = ThePlayer:GetPosition()
+	return TheSim:FindEntities(pos.x,0,pos.z,70,{"_inventoryitem"},{"INLIMBO","flying"})[1]
+end
+
 
 function GroundChestUI:OnUpdate(dt)
 	self:HandleMouseMovement()
+	local item = self:GetNearestItem()
+	if item then
+		local item_atlas = GetInventoryItemAtlas(item.prefab..".tex")
+		self.test_tile:SetItem(item,item_atlas,item.prefab..".tex")
+	end
 end
 
 if ui_button and ui_button ~= 0 then
