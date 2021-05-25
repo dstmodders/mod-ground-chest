853718b29abb0dfac8a3eca91238379b1133ea34
sauktux <64164791+sauktux@users.noreply.github.com>
2021-05-25 14:55:27 +0300

    Upped version to v1.2

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 name = "Ground Chest"
 description = "Open a UI to see items on the ground and interact with them."
 author = "sauktux & Viktor"
-version = "v".."1.1"
+version = "v".."1.2"
 forumthread = ""
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
